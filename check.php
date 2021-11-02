735c4473eebe17f24e4ab666a4131a837783cdfb
csmchika <Alex555dog@mail.ru>
2021-11-03 01:32:56 +0300

    2 апишник сделал, ради наглядности сделал форму и таблицу barcodes - в ней баркоды тех, кто забронировал, поэтому проверка на 2 апишнике происходит потом(представим, что если бы он работал, то там же есть своя база этих barкодов)

--- check.php
+++ check.php
@@ -1,23 +1,2 @@
-<?php
-$data['event_id'] = '003';
-$data['event_date'] = '2021-08-21';
-$data['ticket_adult_price'] = '700';
-$data['ticket_adult_quantity'] ='1';
-$data['ticket_kid_price'] = '450';
-$data['ticket_kid_quantity'] = '0';
-$data['barcode'] = '114239781';
-$ch = curl_init('https://api.site.com/book');
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
 
-$response= curl_exec($ch);
-curl_close($ch);
-$json = '{"error": "order successfully booked"}';
-$res = json_decode($json, true);
-print_r($res);
-//$res = json_encode($response, JSON_UNESCAPED_UNICODE);
-if ($res['message'] == "order successfully booked") {
-    echo 1;
-} else {
-    echo 2;
-}
+
--- test.php
+++ test.php
@@ -1,11 +1,21 @@
 <?php
-//event_id, event_date, ticket_adult_price, ticket_adult_quantity, ticket_kid_price, ticket_kid_quantity
+$data['event_id'] = $_POST['event_id'];
+$data['event_date'] = $_POST['event_date'];
+$data['ticket_adult_price'] = $_POST['ticket_adult_price'];
+$data['ticket_adult_quantity'] = $_POST['ticket_adult_quantity'];
+$data['ticket_kid_price'] = $_POST['ticket_kid_price'];
+$data['ticket_kid_quantity'] = $_POST['ticket_kid_quantity'];
 /**
  * @throws Exception
  */
 function task1()
 {
-    $barcode = createBarcode(dbconnect());
+    global $data;
+    createBarcode(dbconnect());
+    if (checkSecondAPI($data)) {
+        print_r($data);
+//        saveOrder();
+    }
 
 
 
@@ -18,11 +28,14 @@ function dbconnect($dbname="test", $host="localhost", $username="root", $pass="r
 /**
  * @throws Exception
  */
-function createBarcode($db): int
+function createBarcode($db)
 {
     do {
         // Генерируем код
         $number = random_int(100, 99999999);
+        if (checkFirstAPI($number)) {
+            return 0;
+        }
         echo $number;
         // Проверяем в БД
         $sth = $db->prepare("SELECT * FROM `test`.`barcodes` WHERE `barcode` = ?");
@@ -38,9 +51,45 @@ function createBarcode($db): int
     // Сохранение уникального кода
     $sth = $db->prepare("INSERT INTO `test`.`barcodes` SET `barcode` = ?");
     $sth->execute(array($number));
-    return $number;
 }
 
+function checkFirstAPI($barcode): bool
+{
+    global $data;
+    $data['barcode'] = $barcode;
+
+    $ch = curl_init('https://api.site.com/book');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+
+    $response= curl_exec($ch);
+    curl_close($ch);
+    $json = '{message": "order successfully booked"}';
+    $res = json_decode($json, true);
+//    $res = json_decode($response, true);
+    echo 1;
+    return ($res['message'] == "order successfully booked");
+}
+
+function checkSecondAPI($barcode): bool
+{
+    $ch = curl_init('https://api.site.com/approve');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $barcode);
+
+    $response= curl_exec($ch);
+    curl_close($ch);
+    $json = '{"message": "order successfully aproved"}';
+    $res = json_decode($json, true);
+//    $res = json_decode($response, true);
+    echo 2;
+    return ($res['message'] == "order successfully aproved");
+}
+
+function saveOrder($event_id, $event_date, $ticket_adult_price, $ticket_adult_quantity, $ticket_kid_price, $ticket_kid_quantity, $barcode, $db)
+{
+
+}
 try {
     task1();
 } catch (Exception $e) {
