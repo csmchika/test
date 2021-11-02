d1adb2fcd6dd143ad4df4210a1c21ec6e9ea5f34
csmchika <Alex555dog@mail.ru>
2021-11-03 00:26:24 +0300

    Пересмотрел задание, сначала создается баркод, потом сразу надо проверять api, нормальный запрос на api так и не делался, поэтому тестировал со строками, времени много потерял

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
-<form action="welcome.php" method="post">
-    <input type="text" name="first_name" placeholder="Name"><br>
-    <input type="text" name="second_name" placeholder="Surname"><br>
-    <input type="number" name="age" placeholder="Age"><br>
-    <input type="submit" value="Send with POST method">
+<form action="test.php" method="post">
+    <input type="number" name="event_id" placeholder="event_id"><br>
+    <input type="text" name="event_date" placeholder="event_date"><br>
+    <input type="number" name="ticket_adult_price" placeholder="ticket_adult_price"><br>
+    <input type="number" name="ticket_adult_quantity" placeholder="ticket_adult_quantity"><br>
+    <input type="number" name="ticket_kid_price" placeholder="ticket_kid_price"><br>
+    <input type="number" name="ticket_kid_quantity" placeholder="ticket_kid_quantity"><br>
+    <input type="submit" value="Отправить в функцию">
 </form>
 <?php
